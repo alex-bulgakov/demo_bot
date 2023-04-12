dbbe9142fc170d681099e20b467658b15803174f
alebul <master8423@gmail.com>
2023-04-12 21:28:29 +0300

    clear template for bots

--- src/main/java/com/bulgakov/demo_bot/config/BotConfig.java
+++ src/main/java/com/bulgakov/demo_bot/config/BotConfig.java
@@ -1,10 +1,12 @@
 package com.bulgakov.demo_bot.config;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@Data
 @PropertySource("application.properties")
 public class BotConfig {
 
---
+++ src/main/java/com/bulgakov/demo_bot/service/TelegramBot.java
@@ -0,0 +1,29 @@
+package com.bulgakov.demo_bot.service;
+
+import com.bulgakov.demo_bot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
